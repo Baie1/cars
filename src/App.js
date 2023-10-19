db1ab5939c8dfc695113e5e6879b3206f982ba32
baie1 <valenteylor@gmail.com>
2023-10-20 00:49:57 +0600

    cars

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './styles/global.css'
+function App() {
+  return (
+    <div className="App">
+      <div className="logos">
+        <div className="logo-andtxt">
+            <img src="https://seeklogo.com/images/M/mercedes-benz-logo-3B2DDE0497-seeklogo.com.png" alt="" className="mers"/>
+            <h2 className="benz">Mercedes-Benz</h2>
+        </div>
+        <h1 className='txt'>MERCEDES-BENZ AMG CLS </h1>
+      </div>
+        <div className="info-car">
+            <div className="info">
+                 <p className="desc">Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Dolor earum
+            explicabo iste maiores natus numquam officia
+            omnis, possimus praesentium, quia quo ratione
+            recusandae repellendus tenetur ut, velit veritatis
+            voluptate. Consequuntur cumque cupiditate deserunt,
+            dignissimos dolores eveniet, facere facilis fugiat
+            illo praesentium quam velit vitae? Consequuntur
+            ea facilis nam pariatur quibusdam quos soluta,
+            unde? Ab animi corporis sequi? Accusamus delectus
+            deleniti doloribus nam praesentium quam recusanda
+            e reprehenderit soluta ullam ut. Alias cupiditate
+            dolore facere illo minima nam reiciendis repudiandae!
+            Delectus, rem? павпвафв ав аваф а вафавфв
+            авыфавыфавававававфававфав
+            равмпы</p>
+            <h1 className="details">DETAILS</h1>
+        <div className="numbers">
+            <div className="price">
+                <h1 className='five'>520</h1>
+                <p className='text'>Nm</p>
+            </div>
+            <div className="price">
+                <h1 className='five'>320</h1>
+                <p className='text'>kw</p>
+            </div>
+            <div className="price">
+                <h1 className='five'>4.5</h1>
+                <p className='text'>0-100 km/h</p>
+            </div>
+        </div>
+            </div>
+            <div className="car">
+                <img src="https://pngimg.com/d/mercedes_PNG80189.png" alt="" className="car"/>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
